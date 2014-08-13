456d245a34b3a8f624c69304e8efa0bce24365cd
Sean <sean@n2c7.com>
2014-08-13 18:41:05 -0500

    Made Hat Selector

--- src/main/java/me/coldguy101/HubHUD/menus/HatSelectorMenu.java
+++ src/main/java/me/coldguy101/HubHUD/menus/HatSelectorMenu.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 /**
  * Created by Sean on 8/4/2014.
@@ -30,19 +29,19 @@ public class HatSelectorMenu
 		hatSelector.setItem(4, new ItemStack(Material.WATER));
 		hatSelector.setItem(5, new ItemStack(Material.LAVA));
 		hatSelector.setItem(6, new ItemStack(Material.GRAVEL));
-		hatSelector.setItem(7, new ItemStack(Material.LOG));
+		hatSelector.setItem(7, new ItemStack(Material.SPONGE));
 		hatSelector.setItem(8, new ItemStack(Material.LEAVES));
 
 		//Second Row
-		hatSelector.setItem(9, new ItemStack(Material.SPONGE));
-		hatSelector.setItem(10, new ItemStack(Material.STONE));
-		hatSelector.setItem(11, new ItemStack(Material.STONE));
-		hatSelector.setItem(12, new ItemStack(Material.STONE));
-		hatSelector.setItem(13, new ItemStack(Material.STONE));
-		hatSelector.setItem(14, new ItemStack(Material.STONE));
-		hatSelector.setItem(15, new ItemStack(Material.STONE));
-		hatSelector.setItem(16, new ItemStack(Material.STONE));
-		hatSelector.setItem(17, new ItemStack(Material.STONE));
+		hatSelector.setItem(9, new ItemStack(Material.GLASS));
+		hatSelector.setItem(10, new ItemStack(Material.EMERALD_BLOCK));
+		hatSelector.setItem(11, new ItemStack(Material.BRICK));
+		hatSelector.setItem(12, new ItemStack(Material.TNT));
+		hatSelector.setItem(13, new ItemStack(Material.FIRE));
+		hatSelector.setItem(14, new ItemStack(Material.OBSIDIAN));
+		hatSelector.setItem(15, new ItemStack(Material.WORKBENCH));
+		hatSelector.setItem(16, new ItemStack(Material.GOLD_BLOCK));
+		hatSelector.setItem(17, new ItemStack(Material.DIAMOND_BLOCK));
 
 		p.openInventory(hatSelector);
 	}
@@ -52,13 +51,15 @@ public class HatSelectorMenu
 	{
 		if (evt.getInventory().getName().equals(hatSelector.getName())) // The inventory is our custom Inventory
 		{
-			ItemMeta itemMeta = evt.getCurrentItem().getItemMeta();
+			ItemStack clicked = evt.getCurrentItem();
 			Player player = (Player) evt.getWhoClicked();
 
-			player.sendMessage(ChatUtil.pvpitup + ChatColor.BLUE + "Teleporting you to " + itemMeta.getDisplayName());
+			player.getInventory().setHelmet(clicked);
 
-			evt.setCancelled(true); // Make it so the dirt is back in its original spot
-			player.closeInventory(); // Closes their inventory
+			player.sendMessage(ChatUtil.pvpitup + ChatColor.BLUE + "You now are wearing a hat of " + ChatColor.GOLD + clicked.getType().toString() + ChatColor.BLUE + "!");
+
+			evt.setCancelled(true); // Cancel the event
+			player.closeInventory(); // Closes the inventory
 		}
 	}
 }
